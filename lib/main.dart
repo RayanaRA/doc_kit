874f178203df9568cfe6790df7a1a0882ea51d94
kadirovMuhammadziz7 <kadirovmuhammadaziz79@gmail.com>
2023-11-16 18:00:20 +0600

    is not done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:doc_kit/app/components/container/list_card.dart';
+import 'package:doc_kit/app/views/home%20view/homeView.dart';
 import 'package:doc_kit/app/views/home_page/home_page.dart';
 import 'package:doc_kit/app/views/register/get_started.dart';
 import 'package:doc_kit/app/views/register/sign_up.dart';
@@ -14,14 +15,15 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/getScr',
-      routes: {
-        '/homePage': (context) => const HomePage(),
-        '/list': (context) => const ListCard(),
-        '/getScr': (context) => const GetStarted(),
-        '/signUp': (context) => const SignUp(),
-        '/seachView': (context) => SeachView()
-      },
+      // initialRoute: '/getScr',
+      // routes: {
+      //   '/homePage': (context) => const HomePage(),
+      //   '/list': (context) => const ListCard(),
+      //   '/getScr': (context) => const GetStarted(),
+      //   '/signUp': (context) => const SignUp(),
+      //   '/seachView': (context) => SeachView()
+      // },
+      home: HomeView(),
       debugShowCheckedModeBanner: false,
     );
   }
